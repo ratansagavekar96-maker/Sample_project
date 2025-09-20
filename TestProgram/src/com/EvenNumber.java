6ef66fa77fef0478ded7ce3bcf1c265b7e3446e1
Ratan <ratansagavekar96@gmail.com>
2025-09-20 16:53:01 +0530

    Even number code added

---
+++ TestProgram/src/com/EvenNumber.java
@@ -0,0 +1,16 @@
+
+package com;
+
+public class EvenNumber {
+	
+	public static void main(String[] args) {
+		int n=10;
+		
+		if (n%2==0) {
+			System.out.println("Even Number");
+		}else {
+			System.out.println("its odd number");
+		}
+	}
+
+}
